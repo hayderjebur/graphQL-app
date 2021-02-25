d4ad973a21bde47cf6fcf2f15a3de71e25a79a25
Hayder Jebur <hayderjabur1983@gmail.com>
2021-02-25 10:04:21 -0800

    add Post type

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log('Hello GraphQL')
+import { GraphQLServer } from 'graphql-yoga';
+
+//Type definitions (schema)
+const typeDefs = `
+type Query {
+    add(a:Float!,b:Float!):Float!
+ post:Post
+}
+type Post {
+    id:ID!
+    title:String!
+    body:String!
+    published:Int!
+}
+`;
+
+//Resolvers
+const resolvers = {
+  Query: {
+    post() {
+      return {
+        id: '12',
+        title: 'book',
+        body: 'this is a good book',
+        published: 1982,
+      };
+    },
+    add(perant, args, crx, info) {
+      return args.a + args.b;
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log('Server running');
+});
